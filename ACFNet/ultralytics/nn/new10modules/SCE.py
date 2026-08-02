c252efe95198d1eda0920d4e6abc1bac8cb49319
kaikaichen66 <chenkaikai@xynu.edu.cn>
2026-08-02 14:58:30 +0800

    Update SCE.py

--- ACFNet/ultralytics/nn/new10modules/SCE.py
+++ ACFNet/ultralytics/nn/new10modules/SCE.py
@@ -81,7 +81,6 @@ class SCE(nn.Module):
 
         return x * c_att + x * s_att
 
-# 基础模块工具
 def autopad(k, p=None, d=1):
     if d > 1: k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]
     if p is None: p = k // 2 if isinstance(k, int) else [x // 2 for x in k]
